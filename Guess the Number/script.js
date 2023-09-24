0928116a02456883a4165f911f42c92fedd1c577
SejalChaudhary29 <sejalch2933@gmail.com>
2023-09-24 23:07:09 +0530

    html and css part done

---
+++ Guess the Number/script.js
@@ -0,0 +1,13 @@
+const submit = document.querySelector('#sub')
+const userInput = document.querySelector('#guessField')
+const guesseSlot = document.querySelector('#guesses')
+const remaining = document.querySelector('#lastResult')
+const lowHigh = document.querySelector('.lowOrHigh')
+const startOver = document.querySelector('.result')
+const randomNumber = parseInt(Math.random()* 100 + 1)
+console.log(randomNumber)
+const p = document.createElement('p')
+
+
+
+
