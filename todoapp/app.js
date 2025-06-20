63a7b7e56bae5d0b23f59e12fe0687a7ba9a6f13
Sejal Chaudhary <sejalch2933@gmail.com>
2025-06-20 16:14:27 +0530

    added delete and Add button functionality codes

--- todoapp/app.js
+++ todoapp/app.js
@@ -1,8 +1,29 @@
  let inp = document.querySelector('input');
  let btn = document.querySelector('button');
  let ul = document.querySelector('ul');
+// adding input code
+btn.addEventListener("click", function(){
+ let item = document.createElement('li');
+//  console.log(inp.value)
+item.innerText = inp.value;
+let delbtn = document.createElement("button");
+delbtn.innerText= "delete";
+delbtn.classList.add("delete");
+item.appendChild(delbtn);
 
-btn.addEventListener('click', function(){
- let item = document.createElement('li')
- item.innerText = inp.value;
+ ul.appendChild(item);
+ inp.value = "";
 })
+// delete button code
+ul.addEventListener("click", function(event){
+   console.log(event.target)
+   if(event.target.nodeName =="BUTTON"){
+       let listItem = event.target.parentElement;
+    listItem.remove();
+       console.log("delete")
+   }else{
+    console.log("do not delete")
+   }
+        
+    })
+
